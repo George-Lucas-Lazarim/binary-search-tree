a81164bcabdf7126a464a4a948694f26859dee1b
George Lucas Lazarim <gll@asusf15ubuntu.tailaac83f.ts.net>
2026-04-18 19:38:22 -0300

    implementingTheGetTreeHeightFunctionAndTheOtherMissingFunctions

--- simpletree.c
+++ simpletree.c
@@ -5,17 +5,37 @@ int main() {
     int n;
 
     srand(time(NULL));
-    struct treeNode* myTree = NULL;
+    struct treeNode* tree = NULL;
 
     printf("\nEnter the number of random elements you want to insert into the tree: ");
     scanf("%d", &n);
     
     for (int i=0; i < n; i++) {
-        myTree = addData(myTree, rand() % 1000);
+        tree = addData(tree, rand() % 1000);
     }
 
     printf("\nPrinting the tree (parent: left child, right child)");
-    printTree(myTree);
+    printTree(tree);
+
     printf("\n\nPrinting the tree by level (level: data in level)\n");
-    printTreeLevel(myTree);
+    printTreeLevel(tree);
+
+    printf("\nEnter a number to search for it: ");
+    scanf("%d", &n);
+    if (searchData(tree, n)) printf("\nNumber found!");
+    else printf("\nThe number was not found!");
+
+    printf("\nLowest value of the tree: %d", minTree(tree));
+    printf("\nGreater value of the tree: %d", maxTree(tree));
+
+    printf("\nEnter a number to delete it: ");
+    scanf("%d", &n);
+    tree = deleteNode(tree, n);
+    printTreeLevel(tree);
+
+    printf("The height of the tree is: %d", getTreeHeight(tree));
+
+    printf("\nDeleting the tree... ");
+    tree = deleteTree(tree);
+    printf("Deleted!\n");
 }
